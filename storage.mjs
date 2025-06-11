68f2c34a29919611b516b44f409a2c94bdbfac53
Or David <ordavidone@gmail.com>
2025-06-11 11:03:08 +0300

    fs import

--- storage.mjs
+++ storage.mjs
@@ -1,5 +1,4 @@
 // Modified storage.mjs with in-memory option for Cloud Run
-import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { Storage } from '@google-cloud/storage';
